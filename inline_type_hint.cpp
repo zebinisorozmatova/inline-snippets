cab7990718325a127c6c2339f98647424b4ded72
Zebiniso Roʻzmatova <zebinisorozmatova@gmail.com>
2026-03-22 17:15:00 +0500

    Create inline_type_hint.cpp

---
+++ inline_type_hint.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Inline Type Hint Example
+int main() {
+    int a = 5;          // [int]
+    double b = 3.14;    // [double]
+    string name = "Ali";// [string]
+
+    cout << a << ", " << b << ", " << name << endl;
+    return 0;
+}
