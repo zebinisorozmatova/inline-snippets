6e3de1434f7b9d5b7ce559b1509c2ff71a2bc8ee
Zebiniso Roʻzmatova <zebinisorozmatova@gmail.com>
2026-03-22 17:14:52 +0500

    Create smart_placeholder.cpp

---
+++ smart_placeholder.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+template<typename T>
+T getPlaceholder() {
+    if constexpr (is_same<T, int>::value) return 0;
+    else if constexpr (is_same<T, double>::value) return 0.0;
+    else if constexpr (is_same<T, string>::value) return "text";
+    else return T{};
+}
+
+int main() {
+    int a = getPlaceholder<int>();
+    double b = getPlaceholder<double>();
+    string c = getPlaceholder<string>();
+
+    cout << a << ", " << b << ", " << c << endl; // 0, 0, text
+    return 0;
+}
